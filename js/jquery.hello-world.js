e6d7ac5d313bc81f7d63515211a2c5241d3ca578
Tracy Rotton <tracy@taupecat.com>
2013-03-17 09:39:32 -0400

    initial commit

---
+++ js/jquery.hello-world.js
@@ -0,0 +1,31 @@
+(function($) {
+
+	$.fn.helloWorld = function( options ) {
+
+		// Establish our default settings
+		var settings = $.extend({
+			text         : 'Hello, World!',
+			color        : null,
+			fontStyle    : null,
+			complete	 : null
+		}, options);
+
+		return this.each( function() {
+			$(this).text( settings.text );
+
+			if ( settings.color ) {
+				$(this).css( 'color', settings.color );
+			}
+
+			if ( settings.fontStyle ) {
+				$(this).css( 'font-style', settings.fontStyle );
+			}
+
+			if ( $.isFunction( settings.complete ) ) {
+				settings.complete.call(this);
+			}
+		});
+
+	}
+
+}(jQuery));
